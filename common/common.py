fe414ff2e9d0cc16aabb67afe11f45d976d138ce
kly <kly@qq.com>
2023-10-26 22:59:35 +0800

    fist push

--- common/common.py
+++ common/common.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import openpyxl
+import datetime
 import xlrd
 from openpyxl import load_workbook
 
@@ -14,131 +15,156 @@ from openpyxl import load_workbook
 
 
 """按sheet名字cell写"""
-def write_to_excel_cell(path, sheetname, row, column, value):
-    file = openpyxl.load_workbook(path)   #加载
+def write_to_excel_cell(file, sheetname, row, column, value):
     work_sheet = file[sheetname]
     work_sheet.cell(row,column,value)  #修改
-    file.save(path) #保存
-
-
+    # file.save(path) #保存
 
 """ row write """
-def row_write_to_excel(path, sheetname, row, column_start, rowlist):
+def row_write_to_excel(file, sheetname, row, column_start, rowlist):
     for i in range(len(rowlist)):
-        write_to_excel_cell(path, sheetname, row, column_start + i, rowlist[i])
+        write_to_excel_cell(file, sheetname, row, column_start + i, rowlist[i])
 
 
 """ column write """
-def column_write_to_excel(path, sheetname, column, row_start, columnlist):
+def column_write_to_excel(file, sheetname, column, row_start, columnlist):
     for i in range(len(columnlist)):
-        write_to_excel_cell(path, sheetname, row_start + i, column, columnlist[i])
+        # print(columnlist[i])
+        write_to_excel_cell(file, sheetname, row_start + i, column, columnlist[i])
+
 
 
 """按sheetname cell读，如果读到公式，将返回公式表达式"""
-def read_from_excel_cell(path, sheetname, row, column):
-    file = openpyxl.load_workbook(path)   #加载
-    # file = xlrd.open_workbook(path)   #加载
+def read_from_excel_cell(file, sheetname, row, column):
     work_sheet = file[sheetname]
     ret = work_sheet.cell(row, column).value
-    file.save(path) #保存
+    # file.save(path) #保存
     return ret
 
 
 """如果读到公式，则返回为公式的值, 而不是公式的表达式"""
-def data_only_read_from_excel_cell_(path, sheetname, row, column):
-    file = openpyxl.load_workbook(path, data_only=True)   #加载
+def data_only_read_from_excel_cell(file, sheetname, row, column):
+    file = openpyxl.load_workbook(file, data_only=True)   #加载
     # file = xlrd.open_workbook(path)   #加载
     work_sheet = file[sheetname]
     ret = work_sheet.cell(row, column).value
-    file.save(path) #保存
+    # file.save(path) #保存
     return ret
 
-
-
 """ row read """
-def row_read_from_excel(path, sheetname, row, column_start, num, row_val_list):
+def row_read_from_excel(file, sheetname, row, column_start, num, row_val_list):
     for i in range(num):
-        row_val_list.append(read_from_excel_cell(path, sheetname, row, column_start + i))
+        row_val_list.append(read_from_excel_cell(file, sheetname, row, column_start + i))
         # print(row_val_list[i])
 
 
 """ column read """
-def column_read_from_excel(path, sheetname, column, row_start, num, column_val_list):
+def column_read_from_excel(file, sheetname, column, row_start, num, column_val_list):
     for i in range(num):
-        column_val_list.append(read_from_excel_cell(path, sheetname, row_start + i, column))
+        column_val_list.append(read_from_excel_cell(file, sheetname, row_start + i, column))
         # print(column_val_list[i])
 
 
 
+
 """ row swag"""
-def row_swag(path, sheetname, row0, row1,column_start, num):
+def row_swag(file, sheetname, row0, row1,column_start, num):
     row_list_0 = []
     row_list_1 = []
-    row_read_from_excel(path, sheetname, row0, column_start, num, row_list_0)
-    row_read_from_excel(path, sheetname, row1, column_start, num, row_list_1)
+    row_read_from_excel(file, sheetname, row0, column_start, num, row_list_0)
+    row_read_from_excel(file, sheetname, row1, column_start, num, row_list_1)
 
     for i in range(num):
         tmp = row_list_0[i]
         row_list_0[i] = row_list_1[i]
         row_list_1[i] = tmp
 
-    row_write_to_excel(path, sheetname, row0, column_start, row_list_0)
-    row_write_to_excel(path, sheetname, row1, column_start, row_list_1)
-    return
-
+    row_write_to_excel(file, sheetname, row0, column_start, row_list_0)
+    row_write_to_excel(file, sheetname, row1, column_start, row_list_1)
 
 
 """ column swag"""
-def column_swag(path, sheetname, column0, column1, row_start, num):
+def column_swag(file, sheetname, column0, column1, row_start, num):
     column_list_0 = []
     column_list_1 = []
-    column_read_from_excel(path, sheetname, column0, row_start, num, column_list_0)
-    column_read_from_excel(path, sheetname, column1, row_start, num, column_list_1)
+    column_read_from_excel(file, sheetname, column0, row_start, num, column_list_0)
+    column_read_from_excel(file, sheetname, column1, row_start, num, column_list_1)
 
     for i in range(num):
         tmp = column_list_0[i]
         column_list_0[i] = column_list_1[i]
         column_list_1[i] = tmp
 
-    column_write_to_excel(path, sheetname, column0, row_start, column_list_0)
-    column_write_to_excel(path, sheetname, column1, row_start, column_list_1)
-    return
+    column_write_to_excel(file, sheetname, column0, row_start, column_list_0)
+    column_write_to_excel(file, sheetname, column1, row_start, column_list_1)
+
 
 
 
 
 """ row copy"""
-def row_copy(path, sheetname, row_source, row_target,column_start, num):
+def row_copy(file, sheetname, row_source, row_target,column_start, num):
     row_list_tmp = []
-    row_read_from_excel(path, sheetname, row_source, column_start, num, row_list_tmp)
-    row_write_to_excel(path, sheetname, row_target, column_start, row_list_tmp)
-    return
+    row_read_from_excel(file, sheetname, row_source, column_start, num, row_list_tmp)
+    row_write_to_excel(file, sheetname, row_target, column_start, row_list_tmp)
+
 
 """ column copy"""
-def column_copy(path, sheetname, column_source, column_target, row_start, num):
+def column_copy(file, sheetname, column_source, column_target, row_start, num):
     column_list_tmp = []
-    column_read_from_excel(path, sheetname, column_source, row_start, num, column_list_tmp)
-    column_write_to_excel(path, sheetname, column_target, row_start, column_list_tmp)
-    return
+    column_read_from_excel(file, sheetname, column_source, row_start, num, column_list_tmp)
+    column_write_to_excel(file, sheetname, column_target, row_start, column_list_tmp)
+
+
+def del_row_in_excel(file, sheetname, to_del_row_list):
+    work_sheet = file[sheetname]
+    for i in range(len(to_del_row_list)):
+        work_sheet.delete_rows((to_del_row_list[i]-i)) #删除后下边的单元格上移动
+
+
+def del_column_in_excel(file, sheetname, to_del_column_list):
+    work_sheet = file[sheetname]
+    for i in range(len(to_del_column_list)):
+        work_sheet.delete_cols((to_del_column_list[i]-i)) #删除后下边的单元格上移动
 
 
 """字符串查找  查找该字符串的单元格位置和内容"""
-def find_string_in_excel(file_path, sheetname, target_string, resultlist):
-    file = load_workbook(filename=file_path)
+# def find_string_in_excel(file, sheetname, target_string, result_row_list, result_column_list):
+#     sheet = file[sheetname]
+#     for row in sheet.iter_rows():
+#         for cell in row:
+#             if cell.value == target_string:
+#                 print("行：%d 列：%d" %(cell.row, cell.column))
+#                 result_row_list.append(cell.row)
+#                 result_column_list.append(cell.column)
+
+def find_string_in_excel(file, sheetname, target_string, result_row_list, result_column_list):
     sheet = file[sheetname]
     for row in sheet.iter_rows():
         for cell in row:
-            if cell.value == target_string:
-                resultlist.append(cell.row)
-                resultlist.append(cell.column)
+            if target_string in str(cell.value):
+                # print("行：%d 列：%d" %(cell.row, cell.column))
+                result_row_list.append(cell.row)
+                result_column_list.append(cell.column)
+            # else:
+            #     print("查无此人：%s",target_string)
 
 
 
+
+class SysTime:
+    def __init__(self):
+        self.now = datetime.datetime.now()
+        self.year = self.now.year
+        self.month = self.now.month
+        self.day = self.now.day
+        self.hour = self.now.hour
+        self.minute = self.now.minute
 
 
 class SumExcel:
-    def __init__(self, path, sheetname, target_c_or_r, start_of_target, num):
-        self.path = path
+    def __init__(self, file, sheetname, target_c_or_r, start_of_target, num):
+        self.file = file
         self.sheetname = sheetname
         self.target = target_c_or_r
         self.start = start_of_target
@@ -147,7 +173,7 @@ class SumExcel:
         self.tmp = 0
 
     def return_column_sum(self):
-        column_read_from_excel(self.path, self.sheetname, self.target, self.start, self.num, self.cachelist)
+        column_read_from_excel(self.file, self.sheetname, self.target, self.start, self.num, self.cachelist)
         for i in range(len(self.cachelist)):
             if self.cachelist[i] == None:
                 self.cachelist[i] = 0  
@@ -156,7 +182,7 @@ class SumExcel:
         return self.tmp
 
     def return_row_sum(self):
-        row_read_from_excel(self.path, self.sheetname, self.target, self.start, self.num, self.cachelist)
+        row_read_from_excel(self.file, self.sheetname, self.target, self.start, self.num, self.cachelist)
         for i in range(len(self.cachelist)):
             if self.cachelist[i] == None:
                 self.cachelist[i] = 0  
@@ -166,6 +192,246 @@ class SumExcel:
 
 
 
+class GetSearchInExcel:
+    def __init__(self, wb, sheet_name, str ):
+        self.wb = wb
+        self.sheet_name = sheet_name
+        self.str = str
+        self.row_val_list = []
+        self.column_val_list = []
+
+    #找一个最大行，列的字符串,用于搜索    
+    def get_max_row(self):
+        find_string_in_excel(self.wb, self.sheet_name, self.str , self.row_val_list, self.column_val_list)
+        return self.row_val_list[-1]
+
+    def get_max_column(self):
+        find_string_in_excel(self.wb, self.sheet_name, self.str , self.row_val_list, self.column_val_list)
+        return self.column_val_list[-1]
+
+
+
+def del_none_row_and_col(wb, sheet_name):
+    work_sheet = wb[sheet_name]
+    to_del_row_list = []
+    to_del_col_list = []
+
+    row_val_list = []
+    column_val_list = []
+    maxrow = GetSearchInExcel(wb, sheet_name, "合计")
+    maxrow_num = maxrow.get_max_row()
+    maxcol = GetSearchInExcel(wb, sheet_name, "1-12月合计")
+    maxcol_num = maxcol.get_max_column() +3  #由表格获得,+3是多删除一点空白列
+    print("最大列：", maxcol_num)
+
+
+    find_string_in_excel(wb, sheet_name, "本月预算", row_val_list, column_val_list)
+    print("基地址(本月预算)：行_%d,列_%d" %(row_val_list[0],column_val_list[0]))
+
+    find_string_in_excel(wb, sheet_name, "累计费用", row_val_list, column_val_list)
+    print("基地址(累计费用)：行_%d,列_%d" %(row_val_list[1],column_val_list[1]))
+
+
+    #获取坐标
+    base_row_start = row_val_list[0]
+    base_column_start = column_val_list[0]
+    base_row_end = row_val_list[1]
+    base_column_end = column_val_list[1]
+
+    if row_val_list[0] != row_val_list[1]:
+        print("基地址开始行，获取错误！！！！")
+        return
+    
+    column_size = base_column_end - base_column_start + 1 
+    # print("开始行：%d, 最大行：%d" %(base_row_start, maxrow_num ))
+
+
+    # print("************************************************")
+    # print("****************开始删除表格********************")
+
+    for row_num in range(base_row_start, maxrow_num):
+
+        flag_all_none = 0
+        for i in range(base_column_start, (base_column_end +1)):
+            # cell = work_sheet.cell(row=row_num, column=i)
+            tmp = work_sheet.cell(row_num, i).value
+            # print(tmp)
+            if tmp == None or tmp == 0:
+                flag_all_none += 1
+                
+        if flag_all_none == column_size:
+            to_del_row_list.append(row_num)
+        # del_row_list.append(row_num)
+            # tmp1 = work_sheet.cell(row, base_column_start - 1).value
+            # if tmp1 == "小计":
+            #     tmp2 = work_sheet.cell(row, base_column_start - 2).value
+            #     if tmp2 != None:
+            #         work_sheet.delete_rows(row - flag_has_been_del)
+            #         flag_has_been_del += 1
+            #     else:
+            #         work_sheet.delete_rows(row - flag_has_been_del)
+            #         flag_has_been_del += 1
+
+            # elif tmp1 != "小计":
+            #     tmp3 = work_sheet.cell(row, base_column_start - 2).value
+            #     if tmp3 != None:
+            #         row_copy(wb, sheet_name, row, row+1, (base_column_start - 2), 1)
+            #         work_sheet.delete_rows(row - flag_has_been_del)
+            #         flag_has_been_del += 1
+            #     else:
+            #         row_copy(wb, sheet_name, row, row+1, (base_column_start - 2), 1)
+            #         work_sheet.delete_rows(row - flag_has_been_del)
+            #         flag_has_been_del += 1                
+    # print(to_del_row_list)
+    del_row_in_excel(wb, sheet_name, to_del_row_list)
+
+
+
+    del_col_start = base_column_end + 2
+    del_col_end = maxcol_num 
+    for i in range(del_col_start, del_col_end + 1):
+        to_del_col_list.append(i)
+
+    del_column_in_excel(wb, sheet_name, to_del_col_list) 
+    # print("删除空白行，列成功！") 
+
+
+def write_sum_to_xiaoji_row(wb, sheet_name):
+    list_ABC= [ {'1': 'A', 
+                 '2': 'B', 
+                 '3': 'C',
+                 '4': 'D',
+                 '5': 'E',
+                 '6': 'F',
+                 '7': 'G',
+                 '8': 'H',
+                 '9': 'I'}]
+
+    work_sheet = wb[sheet_name]
+
+    row_base_addr_list = []
+    column_base_addr_list = []
+    xioaji_and_tushu_row_list = []
+
+    maxrow = GetSearchInExcel(wb, sheet_name, "合计")
+    maxrow_num = maxrow.get_max_row()
+
+    find_string_in_excel(wb, sheet_name, "本月预算", row_base_addr_list, column_base_addr_list)
+    # print("基地址(本月预算)：行_%d,列_%d" %(row_base_addr_list[0],column_base_addr_list[0]))
+    col_base_start = column_base_addr_list[0]
+
+    for column in range(col_base_start,col_base_start + 5):
+        row_start = row_base_addr_list[0] +1
+        row_end = None
+        for i in range(row_base_addr_list[0] +1, maxrow_num):
+            tmp1 = work_sheet.cell(i, column_base_addr_list[0] - 1).value
+            if tmp1 == "小计":
+                # print(i)
+                row_end = i - 1
+                value = "=sum(" + str(list_ABC[0][str(column)]) + str(row_start) + ":" + str(list_ABC[0][str(column)]) + str(row_end) +")"
+                # print(value)
+                work_sheet.cell(i,column,value)
+                row_start = i + 1
+            if tmp1 == "图书杂志费":
+                row_start += 1
+
+    #获取小记所在行,并完成每一列小计的合计
+    for i in range(row_base_addr_list[0] +1, maxrow_num):
+        tmp1 = work_sheet.cell(i, column_base_addr_list[0] - 1).value
+        if tmp1 == "小计":
+            xioaji_and_tushu_row_list.append(i)
+        if tmp1 == "图书杂志费":
+            xioaji_and_tushu_row_list.append(i)   
+    # print(xioaji_and_tushu_row_list)
+
+    #写入合计
+    for column in range(col_base_start,col_base_start + 5 ):
+        # print("column:",column)
+        str_to_write = "="
+        for i in range(len(xioaji_and_tushu_row_list)):
+            str_to_write += "+" + str(list_ABC[0][str(column)]) + str(xioaji_and_tushu_row_list[i])
+        # print(str_to_write)
+        work_sheet.cell(maxrow_num,column,str_to_write)
+    # print("合计成功！")
+
+
+def copy_data_from_src(wb, sheet_name):
+    work_sheet = wb[sheet_name]
+    systime = SysTime()
+    month = systime.month
+    now = systime.now
+    print("现在是%d月，进行%d月表格处理" %(month, (int(month) - 1)))
+
+    maxrow = GetSearchInExcel(wb, sheet_name, "合计")
+    maxrow_num = maxrow.get_max_row()
+
+    row_base_addr_list = []
+    column_base_addr_list = []
+    month_row_list = []
+    month_column_list = []
+
+    row_of_month_real_money = 0
+    col_of_month_real_money = 0
+    row_of_month_plan_money = 0
+    col_of_month_plan_money = 0
+
+    month_str = (str(month - 1) + "月")
+
+    find_string_in_excel(wb, sheet_name, "本月预算", row_base_addr_list, column_base_addr_list)
+    find_string_in_excel(wb, sheet_name, month_str, month_row_list, month_column_list)
+    n = 4
+    for i in range(len(month_row_list)):
+        tmp1 = work_sheet.cell(month_row_list[i], month_column_list[i]).value
+
+
+        if tmp1 == (str(month - 1) + "月") and month_row_list[i] ==row_base_addr_list[0]:
+            # print("%d月 ：行_%d,列_%d" %(month, month_row_list[i], month_column_list[i]))
+            row_of_month_real_money = month_row_list[i]
+            col_of_month_real_money = month_column_list[i]
+            row_of_month_plan_money = month_row_list[i]
+            col_of_month_plan_money = month_column_list[i]
+            # print(month_row_list[i]+1)
+
+            # print(maxrow_num - month_row_list[i]-1)
+           
+            column_copy(wb, sheet_name, col_of_month_real_money, n, month_row_list[i]+1, maxrow_num - month_row_list[i]-1)
+            n = n-1
+
+def have_processed_check1(file, sheet_name):
+    wb = openpyxl.load_workbook(file,data_only=True)   #加载
+
+    target_str_row = []
+    target_str_row.append(None)
+    target_str_col = [] 
+
+    find_string_in_excel(wb, sheet_name, "1-12月合计", target_str_row, target_str_col)
+    # print("处理标记行：",target_str_row[0])
+    if target_str_row[0] == None:
+        print("#已为：发送版，无需再次处理")
+        # print("%s已完成：透视表 》》》》》底稿，无需再次处理",file)
+        return -1
+    wb.save(file)
+
+
+
+def process_single_excel(file):
+    sheet_name = "安环部预算执行表"
+    # tmp = have_processed_check1(file, sheet_name)
+    # if tmp == -1:
+    #     return
+    
+    wb = openpyxl.load_workbook(file,data_only=True)   #加载
+    copy_data_from_src(wb, sheet_name)
+    del_none_row_and_col(wb, sheet_name)
+    write_sum_to_xiaoji_row(wb, sheet_name)
+    wb.save(file)
+
+def print_log():
+    print("                 _             _   ")
+    print(" _   _ _   _ ___| |_ __ _ _ __| |_ ")
+    print("| | | | | | / __| __/ _` | '__| __|")
+    print("| |_| | |_| \__ \ || (_| | |  | |_ ")
+    print(" \__,_|\__,_|___/\__\__,_|_|   \__|")
 
 
 
--- test.py
+++ test.py
@@ -3,48 +3,101 @@
 # import common
 from common.common import *
 import common.common
+from config.config import *
 import openpyxl
 import xlrd
+import os
+
+# sheet_name = "安环部预算执行表"
+
+# file = r"1、23年9月份管理费用预算执行表-安全环保部.xlsx"
+# # wb = openpyxl.load_workbook(file)   #加载
+
+# process_single_excel(file)
+
+def process_my_files(fold_path):
+    flag =0
+    file_names = os.listdir(fold_path)
+    for file_name in file_names:
+        file_path = os.path.join(fold_path, file_name) #获取完整的文件路径
+
+        if os.path.isfile(file_path):
+            print("开始处理文件：", file_path)
+            process_single_excel(file_path)
+            print("文件处理成功：", file_path)
+            print("************************************************")
+
+        else:
+            print("忽略文件夹:", file_name)
+        flag += 1
+        print("已经第搞定%d个"% flag)
+        
+    # print("提示：所有文件处理成功，底稿 》》》》发送版")
 
 
-# file    = r'test1.xlsx'
-file    = r'1、23年9月份管理费用预算执行表-安全环保部.xlsx'
-sheet_name = "安环部预算执行表"
-# sheet_name = "功率"
-row_val_list = []
-
-column_val_list =[]
-
-tmp = 0
+print_log()
+process_my_files(FOLD_PATH)
 
 
+# def process_files(folder_path):
+#     # 获取文件夹中的所有文件名
+#     file_names = os.listdir(folder_path)
+    
+#     # 遍历每一个文件名
+#     for file_name in file_names:
+#         # 获取完整的文件路径
+#         file_path = os.path.join(folder_path, file_name)
+        
+#         # 判断是否为文件（而不是文件夹）
+#         if os.path.isfile(file_path):
+#             print("Processing file:", file_path)
+#             # 在这里添加处理文件的代码
+#             # ...
+#         else:
+#             print("Ignoring directory:", file_path)
+
+# # 调用函数，传入文件夹路径
+# process_files("F:/处理9月份表格/2_各部门分表")
 
 
 
-column_copy(file, sheet_name, 17, 4, 6, 118)
 
 
-# file.save(file)
-
-# excel = SumExcel(file, sheet_name, 1, 1, 5)
-# total = excel.return_column_sum()
 
 
-# val = read_from_excel_cell(file, sheet_name, 8, 1)
-# print(val)
-# # column_swag(file, sheet_name, 1, 2, 1, 5)
 
-# result = []
-# print(find_string_in_excel(file, sheet_name, "哈哈", result))
 
-# print(result[0])
-# print(result[1])
 
+
+
+
+# # file    = r'test1.xlsx'
+# file    = r'1、23年9月份管理费用预算执行表-安全环保部.xlsx'
+# sheet_name = "安环部预算执行表"
+# # sheet_name = "功率"
+# # sheet_name = "Sheet1"
+
+# wb = openpyxl.load_workbook(file,data_only=True)   #加载
+# copy_data_from_src(wb, sheet_name)
+# del_none_row_and_col(wb, sheet_name)
+# write_sum_to_xiaoji_row(wb, sheet_name)
+# wb.save(file)
+
+
+
+
+# 如果已经处理，就不处理了
 """
 主程序步骤
 1. 由透视后表格创建各部门新的表格
 2. 各部门新的表格，数据处理，产生底稿
 3. 数据处理后，删除一部分，产生发送版
+
+1. 删除一行
+2. 增减一行
+3. 删除一列
+4. 增加一列
+如果已经处理，就不处理了
 
 
 1. 数据校验
